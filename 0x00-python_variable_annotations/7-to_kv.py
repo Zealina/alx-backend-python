27898e82455403958a1ce9626fcff89f6d21a781
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-03-14 18:00:24 +0100

    Re Complex Annotations

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -7,4 +7,4 @@ from typing import Tuple, Union
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Create a Tuple based on annotation"""
-    return (k, float(v))
+    return (k, float(v * v))
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,9 +2,9 @@
 """Duck type function"""
 
 
-from typing import Sequence, Iterable, List
+from typing import Sequence, Iterable, List, Tuple
 
 
-def element_length(lst: Iterable[Sequence]) -> List[tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Return the length og the list"""
     return [(i, len(i)) for i in lst]
