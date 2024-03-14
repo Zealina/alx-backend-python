beeaa7ee8b0c26d8841ef01ec492972a07ac1054
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-03-14 18:31:03 +0100

    Create Type Safety

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -7,6 +7,7 @@ from typing import Sequence, Union, Any
 
 # The types of the elements of the input are not known
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Return the first element of an Sequence"""
     if lst:
         return lst[0]
     else:
