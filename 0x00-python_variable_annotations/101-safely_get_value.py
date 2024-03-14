534db411bb3838bbcc830a32717b38e2f5d6fac8
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-03-14 18:33:29 +0100

    Safety

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -11,6 +11,7 @@ def safely_get_value(
         dct: Mapping,
         key: Any,
         default: Union[T, None] = None) -> Union[Any, T]:
+    """Create Type Safety"""
     if key in dct:
         return dct[key]
     else:
