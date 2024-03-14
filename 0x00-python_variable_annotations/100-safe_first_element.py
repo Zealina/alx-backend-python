5a684e63a8de5495faa2ba8b653ba6eb75996526
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-03-14 18:11:21 +0100

    None or Optional

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Properly Annotate External code"""
+
+
+from typing import Sequence, Union, Any
+
+
+# The types of the elements of the input are not known
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
