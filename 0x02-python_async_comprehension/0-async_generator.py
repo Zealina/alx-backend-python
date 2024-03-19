18fe332a39386996cf8e874e355967b1318a6e48
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-03-19 12:15:16 +0100

    Checket 1

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,6 +8,6 @@ from typing import AsyncGenerator
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """Yield random number"""
-    for i in range(10):
-        await asyncio.sleep(1)
+    for _ in range(10):
         yield random.uniform(0, 10)
+        await asyncio.sleep(1)
