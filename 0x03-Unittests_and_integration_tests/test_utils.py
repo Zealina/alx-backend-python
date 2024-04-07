bdea2d9a04401870e4d653636d95f52eb10b7907
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-04-07 13:30:24 +0100

    Text Exception clause

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Test utils.py module"""
 
-from parameterized import parameterized # type: ignore
+from parameterized import parameterized  # type: ignore
 from typing import Any, Mapping, Sequence
 import unittest
 access_nested_map = __import__("utils").access_nested_map
@@ -14,6 +14,22 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
         ])
-    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected: Any) -> Any:
+    def test_access_nested_map(
+            self,
+            nested_map: Mapping,
+            path: Sequence,
+            expected: Any) -> None:
         """test the method"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+        ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Mapping,
+            path: Sequence) -> None:
+        """Test access nested map exception"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
