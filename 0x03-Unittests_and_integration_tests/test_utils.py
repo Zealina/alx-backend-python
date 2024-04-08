3047b5b19d65c547dcb5a1f3af3ad9b8c0081381
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-04-08 08:09:16 +0100

    Patch Object test case

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,6 +7,7 @@ import unittest
 from unittest.mock import patch, MagicMock
 access_nested_map = __import__("utils").access_nested_map
 get_json = __import__("utils").get_json
+memoize = __import__("utils").memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -55,3 +56,22 @@ class TestGetJson(unittest.TestCase):
         mock_request.return_value = mock_json
         self.assertEqual(get_json(url), test_payload)
         mock_request.assert_called_once()
+
+
+class TestMemoize(unittest.TestCase):
+    """utils.memoize testcases for wrapper function"""
+    def test_memoize(self):
+        """Test the memoize function"""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, 'a_method') as mock_a_method:
+            mock_a_method.return_value = 42
+            inst = TestClass()
+            self.assertEqual(inst.a_property, 42)
+            self.assertEqual(inst.a_property, 42)
+            mock_a_method.assert_called_once()
